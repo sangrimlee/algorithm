2c47e1ca7325c4fb4a2baff0b8b7bdfe57c8273a
sangrimlee <contact@sangrim.dev>
2022-05-31 15:48:15 +0900

    :pencil2: 2022-05-31

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 1 Day 1 Solve (1일 1문제)를 실천하는 Repository입니다.
 
 ## 문제 정리
+
+<details>
+<summary>2022년 5월</summary>
+
+### 2022년 5월
+
+- 2022년 5월 31일
+  - [1461. Check If a String Contains All Binary Codes of Size K](./src/2022-05/leetcode-1461.ts)
+
+</details>
---
+++ src/2022-05/leetcode-1461.ts
@@ -0,0 +1,29 @@
+/*
+  1461. Check If a String Contains All Binary Codes of Size K
+  https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+*/
+
+function hasAllCodes(s: string, k: number): boolean {
+  const n = s.length;
+  const isExist = new Map<number, boolean>();
+  const maxValue = (1 << k) - 1;
+
+  let total = 1 << k;
+  let currentValue = 0;
+
+  for (let i = 0; i < n; i++) {
+    currentValue = ((currentValue << 1) & maxValue) | +s[i];
+
+    if (k <= i + 1 && !isExist.get(currentValue)) {
+      isExist.set(currentValue, true);
+      total -= 1;
+      if (total === 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+console.log(hasAllCodes('00110110', 2));
