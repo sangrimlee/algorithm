ff5befa552d19d07aa0a1c0c67ea2c3d36dc8ec1
sangrimlee <contact@sangrim.dev>
2022-06-01 18:41:48 +0900

    :pencil2: 2022-06-10
    1480. Running Sum of 1d Array
    https://leetcode.com/problems/running-sum-of-1d-array/

--- README.md
+++ README.md
@@ -13,3 +13,13 @@
   - [1461. Check If a String Contains All Binary Codes of Size K](./src/2022-05/leetcode-1461.ts)
 
 </details>
+
+<details>
+<summary>2022년 6월</summary>
+
+### 2022년 6월
+
+- 2022년 6월 1일
+  - [1480. Running Sum of 1d Array](./src/2022-06/leetcode-1480.ts)
+
+</details>
---
+++ src/2022-06/leetcode-1480.ts
@@ -0,0 +1,30 @@
+/*
+  1480. Running Sum of 1d Array
+  https://leetcode.com/problems/running-sum-of-1d-array/
+*/
+
+function runningSum(nums: number[]): number[] {
+  /* Iteration
+  const ret: number[] = [];
+  let previousValue = 0;
+  nums.forEach((num) => {
+    previousValue += num;
+    ret.push(previousValue);
+  });
+  return ret;
+  */
+
+  /* Use reduce */
+  function reduceFunc(
+    [result, previousValue]: [number[], number],
+    currentValue: number
+  ): [number[], number] {
+    return [
+      [...result, previousValue + currentValue],
+      previousValue + currentValue,
+    ];
+  }
+  return nums.reduce(reduceFunc, [[], 0])[0];
+}
+
+console.log(runningSum([1, 2, 3, 4]));
