8f1664410b33f4aa1f271452d40c29ba01e0d725
sangrimlee <contact@sangrim.dev>
2022-07-10 12:15:13 +0900

    :pencil2: 2022-07-10
    - 746. Min Cost Climbing Stairs
    - https://leetcode.com/problems/min-cost-climbing-stairs/

---
+++ src/2022-07/10/leetcode-746.ts
@@ -0,0 +1,14 @@
+/**
+ * 746. Min Cost Climbing Stairs
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ */
+export function minCostClimbingStairs(cost: number[]): number {
+  const n = cost.length;
+  const dp = new Array(n).fill(Number.MAX_SAFE_INTEGER);
+  dp[0] = cost[0];
+  dp[1] = cost[1];
+  for (let i = 2; i < n; i++) {
+    dp[i] = cost[i] + Math.min(dp[i - 2], dp[i - 1]);
+  }
+  return Math.min(dp[n - 2], dp[n - 1]);
+}
