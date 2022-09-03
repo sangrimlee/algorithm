a39de3404854da8835278d4e461a97845ad95e52
sangrimlee <contact@sangrim.dev>
2022-09-03 13:24:24 +0900

    :pencil2: 2022-09-03
    - 8. String to Integer (atoi)
    - https://leetcode.com/problems/string-to-integer-atoi/submissions/
    - 967. Numbers With Same Consecutive Differences
    - https://leetcode.com/problems/numbers-with-same-consecutive-differences/

---
+++ src/2022-09/03/leetcode-967.ts
@@ -0,0 +1,26 @@
+/**
+ * 967. Numbers With Same Consecutive Differences
+ * https://leetcode.com/problems/numbers-with-same-consecutive-differences/
+ */
+export function numsSameConsecDiff(n: number, k: number): number[] {
+  const answer: number[] = [];
+
+  function dfs(current: string) {
+    if (current.length === n) {
+      answer.push(+current);
+      return;
+    }
+    const prev = +current[current.length - 1];
+    for (let i = 0; i < 10; i++) {
+      if (Math.abs(prev - i) === k) {
+        dfs(current + i);
+      }
+    }
+  }
+
+  for (let i = 1; i < 10; i++) {
+    dfs(i.toString());
+  }
+
+  return answer;
+}
