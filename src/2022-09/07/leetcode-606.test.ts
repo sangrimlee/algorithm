ec2be239bdfd94ea9b66f2fcb2f8af5293112a13
sangrimlee <contact@sangrim.dev>
2022-09-07 21:13:19 +0900

    :pencil2: 2022-09-07
    - 606. Construct String from Binary Tree
    - https://leetcode.com/problems/construct-string-from-binary-tree/

---
+++ src/2022-09/07/leetcode-606.test.ts
@@ -0,0 +1,16 @@
+import { createTree } from '../../lib/create-tree';
+import { tree2str } from './leetcode-606';
+
+describe('2022-09-07: LeetCode 606', () => {
+  test('Example 1', () => {
+    const root = createTree([1, 2, 3, 4]);
+
+    expect(tree2str(root)).toBe('1(2(4))(3)');
+  });
+
+  test('Example 2', () => {
+    const root = createTree([1, 2, 3, null, 4]);
+
+    expect(tree2str(root)).toBe('1(2()(4))(3)');
+  });
+});
