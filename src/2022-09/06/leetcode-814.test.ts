547974eac0fc532ca529c41deca91504e6a2bba8
sangrimlee <contact@sangrim.dev>
2022-09-07 09:03:27 +0900

    :pencil2: 2022-09-06
    - 814. Binary Tree Pruning
    - https://leetcode.com/problems/binary-tree-pruning/

---
+++ src/2022-09/06/leetcode-814.test.ts
@@ -0,0 +1,25 @@
+import { compareTree, createTree } from '../../lib/create-tree';
+import { pruneTree } from './leetcode-814';
+
+describe('2022-09-06: LeetCode 814', () => {
+  test('Example 1', () => {
+    const root = createTree([1, 0, null, 0, 1]);
+    const answer = createTree([1, 0, null, null, 1]);
+
+    expect(compareTree(pruneTree(root), answer)).toBeTruthy();
+  });
+
+  test('Example 2', () => {
+    const root = createTree([1, 0, 1, 0, 0, 0, 1]);
+    const answer = createTree([1, null, 1, null, null, null, 1]);
+
+    expect(compareTree(pruneTree(root), answer)).toBeTruthy();
+  });
+
+  test('Example 3', () => {
+    const root = createTree([1, 1, 0, 1, 1, 0, 1, 0]);
+    const answer = createTree([1, 1, 0, 1, 1, null, 1]);
+
+    expect(compareTree(pruneTree(root), answer)).toBeTruthy();
+  });
+});
