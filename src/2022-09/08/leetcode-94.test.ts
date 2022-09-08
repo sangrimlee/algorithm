fa6544080db8d37ad8594f735e44fe946cddf6ae
sangrimlee <contact@sangrim.dev>
2022-09-08 15:09:50 +0900

    :pencil2: 2022-09-08
    - 94. Binary Tree Inorder Traversal
    - https://leetcode.com/problems/binary-tree-inorder-traversal/

---
+++ src/2022-09/08/leetcode-94.test.ts
@@ -0,0 +1,21 @@
+import { createTree } from '../../lib/create-tree';
+import { inorderTraversal } from './leetcode-94';
+
+describe('2022-09-08: LeetCode 94', () => {
+  test('Example 1', () => {
+    const root = createTree([1, 2, 3, 4, null, 5]);
+    expect(inorderTraversal(root)).toEqual([4, 2, 1, 5, 3]);
+  });
+
+  test('Example 2', () => {
+    const root = createTree([]);
+
+    expect(inorderTraversal(root)).toEqual([]);
+  });
+
+  test('Example 3', () => {
+    const root = createTree([1]);
+
+    expect(inorderTraversal(root)).toEqual([1]);
+  });
+});
