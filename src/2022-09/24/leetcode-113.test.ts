0f387111c377b77a91a1bf2407f5a4fd50f6bd77
sangrimlee <contact@sangrim.dev>
2022-09-27 11:13:18 +0900

    :pencil2: 2022-09-24
    - 113. Path Sum II
    - https://leetcode.com/problems/path-sum-ii/

---
+++ src/2022-09/24/leetcode-113.test.ts
@@ -0,0 +1,22 @@
+import { pathSum } from './leetcode-113';
+import { createTree } from '../../lib/create-tree';
+
+describe('2022-09-24 LeetCode 1680', () => {
+  test('Example 1', () => {
+    const root = createTree([1, 2, 3]);
+    const targetSum = 4;
+    expect(pathSum(root, targetSum)).toEqual([[1, 3]]);
+  });
+
+  test('Example 2', () => {
+    const root = createTree([1, 2, 3]);
+    const targetSum = 5;
+    expect(pathSum(root, targetSum)).toEqual([]);
+  });
+
+  test('Example 3', () => {
+    const root = createTree([1, 2]);
+    const targetSum = 0;
+    expect(pathSum(root, targetSum)).toEqual([]);
+  });
+});
