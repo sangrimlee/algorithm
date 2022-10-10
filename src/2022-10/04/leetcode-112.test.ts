522cffd34977588bb3a02598968f8be0448c47a2
sangrimlee <contact@sangrim.dev>
2022-10-10 12:28:14 +0900

    :pencil2: 2022-10-04
    - 112. Path Sum
    - https://leetcode.com/problems/path-sum/

---
+++ src/2022-10/04/leetcode-112.test.ts
@@ -0,0 +1,32 @@
+import { createTree } from '../../lib/create-tree';
+import { hasPathSum } from './leetcode-112';
+
+describe('2022-10-04: LeetCode 112', () => {
+  test('Example 1', () => {
+    const root = createTree([
+      5,
+      4,
+      8,
+      11,
+      null,
+      13,
+      4,
+      7,
+      2,
+      null,
+      null,
+      null,
+      1,
+    ]);
+    expect(hasPathSum(root, 22)).toBe(true);
+  });
+
+  test('Example 2', () => {
+    const root = createTree([1, 2, 3]);
+    expect(hasPathSum(root, 5)).toBe(false);
+  });
+
+  test('Example 1', () => {
+    expect(hasPathSum(null, 0)).toBe(false);
+  });
+});
