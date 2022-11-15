b96f2052695e392aa5e96d2275f5ce5b3fbb7666
sangrimlee <contact@sangrim.dev>
2022-11-15 22:44:26 +0900

    :pencil2: 2022-11-15
    - 222. Count Complete Tree Nodes
    - https://leetcode.com/problems/count-complete-tree-nodes/

---
+++ src/2022-11/15/leetcode-222.test.ts
@@ -0,0 +1,19 @@
+import { countNodes } from './leetcode-222';
+import { createTree } from '../../lib/create-tree';
+
+describe('2022-11-15: Leetcode 222', () => {
+  test('Example 1', () => {
+    const tree = [1, 2, 3, 4, 5, 6];
+    expect(countNodes(createTree(tree))).toBe(tree.length);
+  });
+
+  test('Example 2', () => {
+    const tree: number[] = [];
+    expect(countNodes(createTree(tree))).toBe(tree.length);
+  });
+
+  test('Example 3', () => {
+    const tree = [1];
+    expect(countNodes(createTree(tree))).toBe(tree.length);
+  });
+});
