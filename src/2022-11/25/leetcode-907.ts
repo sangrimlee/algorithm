bd140041601e2e6b285fa55613cce3f0d25255b8
sangrimlee <contact@sangrim.dev>
2022-11-25 12:50:35 +0900

    :pencil2: 2022-11-25
    - 907. Sum of Subarray Minimums
    - https://leetcode.com/problems/sum-of-subarray-minimums/

---
+++ src/2022-11/25/leetcode-907.ts
@@ -0,0 +1,27 @@
+/**
+ * 907. Sum of Subarray Minimums
+ * https://leetcode.com/problems/sum-of-subarray-minimums/
+ */
+export function sumSubarrayMins(arr: number[]): number {
+  const N = arr.length;
+  const MOD = 1_000_000_007;
+  const stack: number[] = [];
+  const dp: number[] = new Array(N).fill(0);
+
+  for (let i = 0; i < N; i++) {
+    while (0 < stack.length && arr[i] < arr[stack[stack.length - 1]]) {
+      stack.pop();
+    }
+
+    if (0 < stack.length) {
+      const smallerIndex = stack[stack.length - 1];
+      dp[i] = dp[smallerIndex] + (i - smallerIndex) * arr[i];
+    } else {
+      dp[i] = (i + 1) * arr[i];
+    }
+    stack.push(i);
+  }
+
+  const answer = dp.reduce((prev, curr) => (prev + curr) % MOD, 0);
+  return answer;
+}
