9313b4d99581d2580607b0f853aa6d13e38a2a77
sangrimlee <contact@sangrim.dev>
2022-12-05 11:03:20 +0900

    :pencil2: 2022-12-05
    - 876. Middle of the Linked List
    - https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ src/2022-12/05/leetcode-876.test.ts
@@ -0,0 +1,17 @@
+import { createLinkedList } from '@lib/create-linked-list';
+
+import { middleNode } from './leetcode-876';
+
+describe('2022-12-05: Leetcode 876', () => {
+  test('Example 1', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const head = createLinkedList(arr);
+    expect(middleNode(head)?.val).toBe(3);
+  });
+
+  test('Example 2', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const head = createLinkedList(arr);
+    expect(middleNode(head)?.val).toBe(4);
+  });
+});
---
+++ src/2022-12/05/leetcode-876.ts
@@ -0,0 +1,23 @@
+import { ListNode } from '@lib/create-linked-list';
+
+/**
+ * 876. Middle of the Linked List
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ */
+export function middleNode(head: ListNode | null): ListNode | null {
+  if (head === null) {
+    return null;
+  }
+
+  if (head.next === null) {
+    return head;
+  }
+
+  let [left, right]: Array<ListNode | null> = [head, head];
+  while (right && right.next) {
+    right = right?.next?.next ?? null;
+    left = left?.next ?? null;
+  }
+
+  return left;
+}
