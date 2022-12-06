90cb4430627710f0b3f1d8cdb696aa0da004215d
sangrimlee <contact@sangrim.dev>
2022-12-06 23:03:49 +0900

    :pencil2: 2022-12-06
    - 328. Odd Even Linked List
    - https://leetcode.com/problems/odd-even-linked-list/description/

---
+++ src/2022-12/06/leetcode-328.test.ts
@@ -0,0 +1,15 @@
+import { createLinkedList, linkedListToArr } from '@lib/create-linked-list';
+import { oddEvenList } from './leetcode-328';
+
+describe('2022-12-06: Leetcode 328', () => {
+  test('Example 1', () => {
+    const head = createLinkedList([1, 2, 3, 4, 5]);
+    expect(linkedListToArr(oddEvenList(head))).toEqual([1, 3, 5, 2, 4]);
+  });
+
+  test('Example 2', () => {
+    const head = createLinkedList([2, 1, 3, 5, 6, 4, 7]);
+
+    expect(linkedListToArr(oddEvenList(head))).toEqual([2, 3, 6, 7, 1, 5, 4]);
+  });
+});
---
+++ src/2022-12/06/leetcode-328.ts
@@ -0,0 +1,24 @@
+import { ListNode } from '@lib/create-linked-list';
+
+/**
+ * 328. Odd Even Linked List
+ * https://leetcode.com/problems/odd-even-linked-list/description/
+ */
+export function oddEvenList(head: ListNode | null): ListNode | null {
+  if (head === null) {
+    return null;
+  }
+
+  const [oddHead, evenHead] = [head, head.next];
+  let [oddNode, evenNode] = [oddHead, evenHead];
+
+  while (evenNode !== null && evenNode.next !== null) {
+    oddNode.next = evenNode.next;
+    oddNode = oddNode.next;
+    evenNode.next = oddNode.next;
+    evenNode = evenNode.next;
+  }
+  oddNode.next = evenHead;
+
+  return oddHead;
+}
