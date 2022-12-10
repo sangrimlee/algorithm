570a6e72f2655825a9412a25e7b2f4e4bf92f42e
sangrimlee <contact@sangrim.dev>
2022-12-10 10:13:21 +0900

    1339. Maximum Product of Splitted Binary Tree
    - 1339. Maximum Product of Splitted Binary Tree
    - https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/

---
+++ src/2022-12/10/leetcode-1339.ts
@@ -0,0 +1,40 @@
+import { TreeNode } from '@lib/create-tree';
+
+/**
+ * 1339. Maximum Product of Splitted Binary Tree
+ * https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+ */
+export function maxProduct(root: TreeNode | null): number {
+  const MOD = 10 ** 9 + 7;
+  if (root === null) {
+    return 0;
+  }
+
+  let answer = 0;
+  const totalValue = getTotalValue(root);
+
+  function findMaxProduct(root: TreeNode | null): number {
+    if (root === null) {
+      return 0;
+    }
+
+    const leftTotalValue = findMaxProduct(root.left);
+    const rightTotalValue = findMaxProduct(root.right);
+    answer = Math.max(
+      answer,
+      (totalValue - leftTotalValue) * leftTotalValue,
+      (totalValue - rightTotalValue) * rightTotalValue
+    );
+    return root.val + leftTotalValue + rightTotalValue;
+  }
+  findMaxProduct(root);
+
+  return answer % MOD;
+}
+
+function getTotalValue(root: TreeNode | null): number {
+  if (root === null) {
+    return 0;
+  }
+  return root.val + getTotalValue(root.left) + getTotalValue(root.right);
+}
