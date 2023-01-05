be01cf26ab4ab29d07a134b6d0b30a98a5621eec
sangrimlee <contact@sangrim.dev>
2023-01-05 18:09:02 +0900

    :pencil2: 2023-01-04
    - 2244. Minimum Rounds to Complete All Tasks
    - https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks

---
+++ solution/src/2023-01/04/leetcode-2244.ts
@@ -0,0 +1,20 @@
+/**
+ * 2244. Minimum Rounds to Complete All Tasks
+ * https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks
+ */
+export function minimumRounds(tasks: number[]): number {
+  const counts = new Map<number, number>();
+  for (const task of tasks) {
+    counts.set(task, (counts.get(task) || 0) + 1);
+  }
+
+  let minimumRound = 0;
+  for (const count of counts.values()) {
+    if (count === 1) {
+      return -1;
+    }
+    minimumRound += Math.floor((count + 2) / 3);
+  }
+
+  return minimumRound;
+}
