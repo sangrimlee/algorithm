d368c7c340ecd771874821415980e6e1373bc7ea
sangrimlee <contact@sangrim.dev>
2023-03-02 13:54:16 +0900

    :pencil2: 2023-03-01
    - 프로그래머스 132265. 롤케이크 자르기

---
+++ solution/src/2023-03/01/programmers-132265.ts
@@ -0,0 +1,26 @@
+/**
+ * 132265. 롤케이크 자르기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/132265
+ */
+export function solution(toppings: number[]): number {
+  const toppingCount = new Map<number, number>();
+  for (const topping of toppings) {
+    toppingCount.set(topping, (toppingCount.get(topping) || 0) + 1);
+  }
+
+  let answer = 0;
+  const currentCount = new Map<number, number>();
+  for (const topping of toppings) {
+    toppingCount.set(topping, (toppingCount.get(topping) || 0) - 1);
+    currentCount.set(topping, (currentCount.get(topping) || 0) + 1);
+
+    if (toppingCount.get(topping) === 0) {
+      toppingCount.delete(topping);
+    }
+    if (currentCount.size === toppingCount.size) {
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
