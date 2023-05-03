cbfe8384509e28c7a26c183fa38dc210517e2c6b
sangrimlee <contact@sangrim.dev>
2023-05-03 19:44:30 +0900

    feat: CodeBlockTemplate을 생성하는 기능 추가

--- packages/cli/src/lib/template/index.ts
+++ packages/cli/src/lib/template/index.ts
@@ -1,3 +1,4 @@
+export { createCodeBlockTemplate } from './code-block';
 export { createREADMETemplate } from './readme';
 export { createSolutionTemplate } from './solution';
 export { createTestTemplate, createTestCaseTemplate } from './test';
