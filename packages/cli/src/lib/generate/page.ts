4302e2a5b2bb30b834173339e26981180f492359
sangrimlee <contact@sangrim.dev>
2023-05-03 20:15:05 +0900

    feat: page meta 생성 기능 추가

--- packages/cli/src/lib/generate/page.ts
+++ packages/cli/src/lib/generate/page.ts
@@ -1,9 +1,9 @@
 import path from 'node:path';
 
-import { getSolutions } from '@/api/solution';
+import { groupByCodingSite, getSolutions } from '@/api/solution';
 import { createSolutionPageTemplate } from '@/lib/template';
-import { ensureWriteFile } from '@/utils/fs';
-import { Solution } from '@/types';
+import { ensureWriteFile, ensureWriteJson } from '@/utils/fs';
+import { CodingSite, Solution } from '@/types';
 import { EXTNAME } from '@/constants';
 
 async function generateSolutionPage(solution: Solution, outDir: string) {
@@ -15,8 +15,24 @@ async function generateSolutionPage(solution: Solution, outDir: string) {
   );
 }
 
+export async function generateSolutionPageMeta(
+  codingSite: CodingSite,
+  solutions: Solution[],
+  outDir: string,
+) {
+  const meta = Object.fromEntries(solutions.map(({ id, title }) => [id, title]));
+
+  await ensureWriteJson(path.join(outDir, codingSite.toLowerCase(), `_meta${EXTNAME.JSON}`), meta);
+}
+
 export async function generatePage(solutionDir: string, outDir: string) {
   const solutions = await getSolutions(solutionDir, outDir);
+  const groups = Array.from(groupByCodingSite(solutions));
 
-  await Promise.all(solutions.map((solution) => generateSolutionPage(solution, outDir)));
+  await Promise.all([
+    ...solutions.map((solution) => generateSolutionPage(solution, outDir)),
+    ...groups.map(([codingSite, solution]) =>
+      generateSolutionPageMeta(codingSite, solution, outDir),
+    ),
+  ]);
 }
