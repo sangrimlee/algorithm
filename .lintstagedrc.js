f98e4ac1a2cb4dcb2a86110aba26e1bdbbb97b6b
이상림 (Sang Rim Lee) <contact@sangrim.dev>
2023-05-03 19:19:41 +0900

    chore: pnpm으로 packageManager 변경 (#8)
    - pnpm으로 packageManger 변경
    - dependencies 업데이트
    - pnpm 및 node 버전 명시

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,9 +3,9 @@ const path = require('path');
 const joinFileNames = (fileNames, delimiter = ' ') =>
   fileNames.map((f) => path.relative(process.cwd(), f)).join(delimiter);
 
-const eslintCommand = (fileNames) => `yarn eslint --fix ${joinFileNames(fileNames)}`;
+const eslintCommand = (fileNames) => `pnpm eslint --fix ${joinFileNames(fileNames)}`;
 
-const prettierCommand = (fileNames) => `yarn prettier --write ${joinFileNames(fileNames)}`;
+const prettierCommand = (fileNames) => `pnpm prettier --write ${joinFileNames(fileNames)}`;
 
 module.exports = {
   '*.{js,ts,json,md}': [prettierCommand],
