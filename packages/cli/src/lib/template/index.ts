2990ee053990fdee4bba5558afffb436116e1c26
sangrimlee <contact@sangrim.dev>
2023-05-03 19:48:06 +0900

    feat: SolutionPage를 생성하는 기능 추가

--- packages/cli/src/lib/template/index.ts
+++ packages/cli/src/lib/template/index.ts
@@ -1,4 +1,5 @@
 export { createCodeBlockTemplate } from './code-block';
+export { createSolutionPageTemplate } from './page';
 export { createREADMETemplate } from './readme';
 export { createSolutionTemplate } from './solution';
 export { createTestTemplate, createTestCaseTemplate } from './test';
