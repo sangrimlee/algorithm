03e830aa6650bb37e704bdf0a8f4a2434f931474
sangrimlee <contact@sangrim.dev>
2023-05-12 14:23:58 +0900

    feat: SEO 관련 설정 변경

--- website/src/theme.config.tsx
+++ website/src/theme.config.tsx
@@ -17,30 +17,51 @@ const config: DocsThemeConfig = {
         titleTemplate: '%s | Algorithm',
       };
     }
+    return {
+      titleTemplate: '%s',
+    };
   },
   logo: <Logo className="nx-mt-1" />,
   head: function useHead() {
-    const { title } = useConfig();
-    const url = process.env.NEXT_PUBLIC_URL;
-    const domain = url.replace(/^https?:\/\//, '');
-    const ogImage = url + '/images/og.jpeg';
-    const description = '';
+    const { asPath } = useRouter();
+    const { title, frontMatter } = useConfig();
+
+    const baseURL = new URL(process.env.NEXT_PUBLIC_URL);
+    const domain = baseURL.hostname;
+    const ogImage = new URL('/images/og.png', baseURL).href;
+    const url = new URL(asPath, baseURL).href;
+    const description = 'TypeScript로 작성한 LeetCode와 Programmers의 문제 풀이 입니다.';
+
+    const tags = [
+      '알고리즘',
+      '코딩테스트',
+      'LeetCode',
+      'Programmers',
+      'TypeScript',
+      ...(frontMatter.tags || []),
+    ];
 
     return (
       <>
-        <meta name="theme-color" content="#fff" />
-        <meta name="msapplication-TileColor" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
+
+        <meta name="author" content="sangrimlee" />
         <meta name="description" content={description} />
+        <meta name="keywords" content={tags.join(', ')} />
+
+        <meta name="og:title" content={title ? title + ' |  Algorithm' : 'Algorithm'} />
         <meta name="og:description" content={description} />
+        <meta name="og:image" content={ogImage} />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="960" />
+        <meta name="og:type" content="website" />
+        <meta name="og:site_name" content="Algorithm" />
+        <meta property="og:locale" content="ko_KR"></meta>
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImage} />
         <meta name="twitter:site:domain" content={domain} />
         <meta name="twitter:url" content={url} />
-        <meta name="og:title" content={title ? title + ' |  Algorithm' : 'Algorithm'} />
-        <meta name="og:image" content={ogImage} />
-        <meta name="apple-mobile-web-app-title" content="Algorithm" />
 
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.png" type="image/png" />
