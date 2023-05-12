1cdd327796107b721dd252c680abe32ba9656ed9
sangrimlee <contact@sangrim.dev>
2023-05-12 16:41:12 +0900

    feat: tailwindcss 스타일로 변경

--- website/src/components/mdx-components/badge.tsx
+++ website/src/components/mdx-components/badge.tsx
@@ -1,37 +1,27 @@
+import clsx from 'clsx';
+
+const levelColors = {
+  easy: 'text-green-600 bg-green-600 dark:text-green-400 dark:bg-green-400',
+  medium: 'text-yellow-600 bg-yellow-600 dark:text-yellow-400 dark:bg-yellow-400',
+  hard: 'text-red-600 bg-red-600 dark:text-red-400 dark:bg-red-400',
+};
+
 interface LevelBadgeProps {
-  level: 'easy' | 'medium' | 'hard';
+  level: keyof typeof levelColors;
 }
 
 export const LevelBadge = ({ level }: LevelBadgeProps) => {
   return (
     <>
-      <div className={`level-badge ${level}`}>{level}</div>
-      <style jsx>{`
-        .level-badge {
-          display: inline-flex;
-          align-items: center;
-          justify-content: center;
-          border-radius: 9999px;
-          padding: 0.25rem 0.875rem;
-
-          font-size: 1rem;
-          font-weight: 500;
-          text-transform: capitalize;
-
-          color: rgb(var(--level-color));
-          background: rgb(var(--level-color) / 0.15);
-        }
-
-        .level-badge.easy {
-          --level-color: 0 175 155;
-        }
-        .level-badge.medium {
-          --level-color: 255 184 0;
-        }
-        .level-badge.hard {
-          --level-color: 255 45 85;
-        }
-      `}</style>
+      <div
+        className={clsx(
+          'inline-flex items-center justify-center rounded-full py-1 px-3.5 font-medium capitalize',
+          'bg-opacity-[0.15] hover:bg-opacity-20 dark:bg-opacity-[0.15] dark:hover:bg-opacity-25',
+          levelColors[level],
+        )}
+      >
+        {level}
+      </div>
     </>
   );
 };
@@ -43,37 +33,18 @@ interface TopicBadgeProps {
 
 export const TopicBadge = ({ name, slug }: TopicBadgeProps) => {
   return (
-    <>
-      <a
-        className="topic-badge"
-        href={`https://leetcode.com/tag/${slug}`}
-        target="_blank"
-        rel="nofollow noreferrer"
-      >
-        {name}
-      </a>
-      <style jsx>{`
-        .topic-badge {
-          display: inline-flex;
-          align-items: center;
-          justify-content: center;
-
-          border-radius: 9999px;
-          padding: 0.25rem 0.875rem;
-
-          font-size: 1rem;
-          font-weight: 500;
-
-          --color-hue: 212deg;
-          --bg-opacity: 0.1;
-          color: hsl(var(--color-hue) 100% 45%);
-          background: hsl(var(--color-hue) 100% 66% / var(--bg-opacity));
-        }
-        .topic-badge:hover {
-          --bg-opacity: 0.25;
-        }
-      `}</style>
-    </>
+    <a
+      className={clsx(
+        'inline-flex items-center justify-center rounded-full py-1 px-3.5 font-medium',
+        'bg-blue-600 text-blue-600 bg-opacity-10 hover:bg-opacity-20',
+        'dark:bg-blue-400 dark:text-blue-400  dark:bg-opacity-[0.15] dark:hover:bg-opacity-25',
+      )}
+      href={`https://leetcode.com/tag/${slug}`}
+      target="_blank"
+      rel="nofollow noreferrer"
+    >
+      {name}
+    </a>
   );
 };
 
@@ -82,18 +53,5 @@ interface BadgesProps {
 }
 
 export const Badges = ({ children }: BadgesProps) => {
-  return (
-    <>
-      <div className="topic-badges">{children}</div>
-      <style jsx>{`
-        .topic-badges {
-          display: flex;
-          align-items: center;
-          gap: 0.75rem;
-          flex-wrap: wrap;
-          margin-top: 1.25rem;
-        }
-      `}</style>
-    </>
-  );
+  return <div className="flex items-center gap-3 flex-wrap mt-5">{children}</div>;
 };
