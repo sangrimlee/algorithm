fa748d4c75d7b83364c18f93537e5e2b9e4b75f1
sangrimlee <contact@sangrim.dev>
2023-05-13 19:18:18 +0900

    feat: Poppins 폰트 추가

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ['var(--font-logo)', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        'object-1': 'inset -30px 0px 35px #89D9F2;',
+      },
+    },
+  },
   plugins: [],
 };
