6d33d9c88f37cc40bd7fa7e9ee072bd5baf9bc8a
sangrimlee <contact@sangrim.dev>
2023-05-16 20:08:33 +0900

    feat: HomePage 애니메이션 추가

--- website/src/components/pages/home/background.tsx
+++ website/src/components/pages/home/background.tsx
@@ -1,17 +1,20 @@
 import clsx from 'clsx';
-import { SVGProps } from 'react';
+
+import { FadeIn } from '@/components/animations';
 
 export const Background = () => {
   return (
     <>
       <div className="absolute inset-0 -z-10">
         <div className="absolute inset-0 bg-grid-gray-200/50 dark:bg-grid-gray-800/50" />
-        <div
-          className={clsx(
-            'gradient absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-48 h-48 opacity-30 blur-3xl animate-pulse',
-            'md:w-96 md:h-96 xl:w-[32rem] xl:h-[32rem] md:blur-[128px]',
-          )}
-        />
+        <FadeIn delay={1} noVertical>
+          <div
+            className={clsx(
+              'gradient absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-48 h-48 opacity-30 blur-3xl animate-pulse',
+              'md:w-96 md:h-96 xl:w-[32rem] xl:h-[32rem] md:blur-[128px]',
+            )}
+          />
+        </FadeIn>
       </div>
       <style jsx>
         {`
--- website/src/components/pages/home/index.tsx
+++ website/src/components/pages/home/index.tsx
@@ -1,14 +1,17 @@
 import clsx from 'clsx';
 
-import { Background } from './background';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
+import { Background } from './background';
+import { FadeIn, ScaleUp, SlideHorizontal } from '@/components/animations';
 
 interface LinkBadgeProps {
   href: string;
   label: string;
 }
+
 const LinkBadge = ({ href, label }: LinkBadgeProps) => {
   return (
     <Link
@@ -25,6 +28,75 @@ const LinkBadge = ({ href, label }: LinkBadgeProps) => {
   );
 };
 
+const TitleAndDescription = () => {
+  return (
+    <div className="flex-1 flex flex-col items-center justify-center pt-36 md:pt-48">
+      <FadeIn>
+        <h1
+          className={clsx(
+            'font-logo text-5xl font-extrabold text-center tracking-tight text-gray-800',
+            'md:text-6xl xl:text-7xl dark:text-gray-200',
+          )}
+        >
+          Algorithm
+        </h1>
+      </FadeIn>
+      <FadeIn delay={0.2}>
+        <p
+          className={clsx(
+            'text-center mt-6 text-gray-600 dark:text-gray-400 max-w-lg px-8',
+            'text-base md:text-xl xl:text-2xl md:mt-8 xl:mt-10 md:max-w-xl xl:max-w-2xl',
+          )}
+        >
+          The solutions written in TypeScript for problems of LeetCode and Programmers.
+        </p>
+      </FadeIn>
+      <FadeIn delay={0.4}>
+        <div className="mt-6 md:mt-10 space-x-4">
+          <LinkBadge href="/solution/leetcode/1" label="LeetCode" />
+          <LinkBadge href="/solution/programmers/1843" label="Programmers" />
+        </div>
+      </FadeIn>
+    </div>
+  );
+};
+
+const Profile = () => {
+  return (
+    <div className="flex items-center py-12 md:py-16">
+      <ScaleUp delay={0.6}>
+        <Image
+          src="/images/profile.png"
+          alt="Profile image"
+          width={512}
+          height={512}
+          className={clsx('w-12 h-12 rounded-full', 'md:w-14 md:h-14 xl:w-16 xl:h-16')}
+        />
+      </ScaleUp>
+      <SlideHorizontal direction="right" delay={0.8}>
+        <div className="flex flex-col ml-3 text-xs font-medium md:text-sm xl:text-base">
+          <a
+            href="https://github.com/sangrimlee"
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+            className="text-blue-600 dark:text-blue-400"
+          >
+            @sangrimlee
+          </a>
+          <a
+            href="mailto:contact@sangrim.dev"
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+            className="mt-1 text-gray-600 dark:text-gray-400 ml-0.5"
+          >
+            contact@sangrim.dev
+          </a>
+        </div>
+      </SlideHorizontal>
+    </div>
+  );
+};
+
 export const HomePage = () => {
   return (
     <main
@@ -35,55 +107,8 @@ export const HomePage = () => {
     >
       <Background />
       <div className="w-full h-full flex flex-col items-center">
-        <div className="flex-1 flex flex-col items-center justify-center pt-36 md:pt-48">
-          <h1
-            className={clsx(
-              'font-logo text-5xl font-extrabold text-center tracking-tight text-gray-800',
-              'md:text-6xl xl:text-7xl dark:text-gray-200',
-            )}
-          >
-            Algorithm
-          </h1>
-          <p
-            className={clsx(
-              'text-center mt-6 text-gray-600 dark:text-gray-400 max-w-lg px-8',
-              'text-base md:text-xl xl:text-2xl md:mt-8 xl:mt-10 md:max-w-xl xl:max-w-2xl',
-            )}
-          >
-            The solutions written in TypeScript for problems of LeetCode and Programmers.
-          </p>
-          <div className="mt-6 md:mt-10 space-x-4">
-            <LinkBadge href="/solution/leetcode/1" label="LeetCode" />
-            <LinkBadge href="/solution/programmers/1843" label="Programmers" />
-          </div>
-        </div>
-        <div className="flex items-center py-12 md:py-16">
-          <Image
-            src="/images/profile.png"
-            alt="Profile image"
-            width={512}
-            height={512}
-            className={clsx('w-12 h-12 rounded-full', 'md:w-14 md:h-14 xl:w-16 xl:h-16')}
-          />
-          <div className="flex flex-col ml-3 text-xs font-medium md:text-sm xl:text-base">
-            <a
-              href="https://github.com/sangrimlee"
-              target="_blank"
-              rel="nofollow noreferrer noopener"
-              className="text-blue-600 dark:text-blue-400"
-            >
-              @sangrimlee
-            </a>
-            <a
-              href="mailto:contact@sangrim.dev"
-              target="_blank"
-              rel="nofollow noreferrer noopener"
-              className="mt-1 text-gray-600 dark:text-gray-400 ml-0.5"
-            >
-              contact@sangrim.dev
-            </a>
-          </div>
-        </div>
+        <TitleAndDescription />
+        <Profile />
       </div>
     </main>
   );
