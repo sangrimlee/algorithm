094b06c7a3a2bf9585b04b62bf0d8ed6f719f0de
sangrimlee <contact@sangrim.dev>
2023-09-08 12:49:17 +0900

    fix: 잘못된 테스트 수정
    - 잘못된 테스트 코드 수정

--- solution/src/2023-07/26/leetcode-1870.test.ts
+++ solution/src/2023-07/26/leetcode-1870.test.ts
@@ -1,11 +1,13 @@
+import { minSpeedOnTime } from './leetcode-1870';
+
 describe('2023-07-26: LeetCode 1870', () => {
   test('Example 1', () => {
-    expect(solution([1, 3, 2], 6)).toEqual(1);
+    expect(minSpeedOnTime([1, 3, 2], 6)).toBe(1);
   });
   test('Example 2', () => {
-    expect(solution([1, 3, 2], 2.7)).toEqual(3);
+    expect(minSpeedOnTime([1, 3, 2], 2.7)).toBe(3);
   });
   test('Example 3', () => {
-    expect(solution([1, 3, 2], 1.9)).toEqual(-1);
+    expect(minSpeedOnTime([1, 3, 2], 1.9)).toBe(-1);
   });
 });
