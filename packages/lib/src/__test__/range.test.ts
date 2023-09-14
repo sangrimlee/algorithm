f18dfba358f7318422b29adb18df3a4b50714f70
sangrimlee <contact@sangrim.dev>
2023-09-14 15:46:03 +0900

    feat: add test for range
    - `range`에 대한 테스트 코드 추가

---
+++ packages/lib/src/__test__/range.test.ts
@@ -0,0 +1,17 @@
+import { range } from '../range';
+
+test('range with end.', () => {
+  expect([...range(10)]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+
+test('range with start and end.', () => {
+  expect([...range(10, 20)]).toEqual([10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+});
+
+test('range with start, end and step.', () => {
+  expect([...range(10, 20, 2)]).toEqual([10, 12, 14, 16, 18]);
+});
+
+test('range with start, end and negative step.', () => {
+  expect([...range(20, 10, -2)]).toEqual([20, 18, 16, 14, 12]);
+});
