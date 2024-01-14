d2a086f196a87007554b000f5952762492e072c9
sangrimlee <contact@sangrim.dev>
2024-01-14 23:40:01 +0900

    :pencil2: 2024-01-14
    - LeetCode 129. Sum Root to Leaf Numbers
    - https://leetcode.com/problems/sum-root-to-leaf-numbers
    - LeetCode 1945. Sum of Digits of String After Convert
    - https://leetcode.com/problems/sum-of-digits-of-string-after-convert

---
+++ solution/src/2024-01/14/leetcode-129.ts
@@ -0,0 +1,28 @@
+import { TreeNode } from '@algorithm/lib';
+
+/**
+ * 129. Sum Root to Leaf Numbers
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers
+ */
+export function sumNumbers(root: TreeNode | null): number {
+  if (root === null) {
+    return 0;
+  }
+
+  let answer = 0;
+  function sumRootToLeaf(node: TreeNode, prev: number) {
+    const current = prev * 10 + node.val;
+    if (node.left === null && node.right === null) {
+      answer += current;
+      return;
+    }
+    if (node.left) {
+      sumRootToLeaf(node.left, current);
+    }
+    if (node.right) {
+      sumRootToLeaf(node.right, current);
+    }
+  }
+  sumRootToLeaf(root, 0);
+  return answer;
+}
---
+++ solution/src/2024-01/14/leetcode-1945.ts
@@ -0,0 +1,21 @@
+/**
+ * 1945. Sum of Digits of String After Convert
+ * https://leetcode.com/problems/sum-of-digits-of-string-after-convert
+ */
+export function getLucky(s: string, k: number): number {
+  function getAlphabetPosition(char: string) {
+    return char.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+  }
+  function convert(s: string) {
+    return [...s].map(getAlphabetPosition).join('');
+  }
+  function transform(s: string) {
+    return [...s].reduce((prev, char) => prev + parseInt(char), 0).toString();
+  }
+
+  let answer = convert(s);
+  for (let i = 1; i <= k; i++) {
+    answer = transform(answer);
+  }
+  return parseInt(answer);
+}
