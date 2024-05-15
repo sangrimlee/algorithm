74b529773c2de6cbf2aabbd0b9d3aa52510e1637
sangrimlee <contact@sangrim.dev>
2024-05-15 17:24:30 +0900

    :pencil2: 2024-05-15
    - LeetCode 2812. Find the Safest Path in a Grid
    - https://leetcode.com/problems/find-the-safest-path-in-a-grid

---
+++ solution/src/2024-05/15/leetcode-2812.test.ts
@@ -0,0 +1,32 @@
+import { maximumSafenessFactor } from './leetcode-2812';
+
+describe('2024-05-15: LeetCode 2812', () => {
+  test('Example 1', () => {
+    expect(
+      maximumSafenessFactor([
+        [1, 0, 0],
+        [0, 0, 0],
+        [0, 0, 1],
+      ]),
+    ).toBe(0);
+  });
+  test('Example 2', () => {
+    expect(
+      maximumSafenessFactor([
+        [0, 0, 1],
+        [0, 0, 0],
+        [0, 0, 0],
+      ]),
+    ).toBe(2);
+  });
+  test('Example 3', () => {
+    expect(
+      maximumSafenessFactor([
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+      ]),
+    ).toBe(2);
+  });
+});
