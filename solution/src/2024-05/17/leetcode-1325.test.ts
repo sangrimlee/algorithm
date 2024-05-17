1689f96c555f494997c620bc908b05888a2a2900
sangrimlee <contact@sangrim.dev>
2024-05-17 10:42:17 +0900

    :pencil2: 2024-05-17
    - LeetCode 1325. Delete Leaves With a Given Value
    - https://leetcode.com/problems/delete-leaves-with-a-given-value

---
+++ solution/src/2024-05/17/leetcode-1325.test.ts
@@ -0,0 +1,20 @@
+import { compareTree, createTree } from '@algorithm/lib';
+import { removeLeafNodes } from './leetcode-1325';
+
+describe('2024-05-17: LeetCode 1325', () => {
+  test('Example 1', () => {
+    const root = createTree([1, 2, 3, 2, null, 2, 4]);
+    const answer = createTree([1, null, 3, null, null, null, 4]);
+    expect(compareTree(removeLeafNodes(root, 2), answer)).toBe(true);
+  });
+  test('Example 2', () => {
+    const root = createTree([1, 3, 3, 3, 2]);
+    const answer = createTree([1, 3, null, null, 2]);
+    expect(compareTree(removeLeafNodes(root, 3), answer)).toBe(true);
+  });
+  test('Example 3', () => {
+    const root = createTree([1, 2, null, 2]);
+    const answer = createTree([1]);
+    expect(compareTree(removeLeafNodes(root, 2), answer)).toBe(true);
+  });
+});
---
+++ solution/src/2024-05/17/leetcode-1325.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from '@algorithm/lib';
+
+/**
+ * 1325. Delete Leaves With a Given Value
+ * https://leetcode.com/problems/delete-leaves-with-a-given-value
+ */
+export function removeLeafNodes(root: TreeNode | null, target: number): TreeNode | null {
+  if (root === null) {
+    return null;
+  }
+  root.left = removeLeafNodes(root.left, target);
+  root.right = removeLeafNodes(root.right, target);
+  if (root.left === null && root.right === null && root.val === target) {
+    return null;
+  }
+  return root;
+}
