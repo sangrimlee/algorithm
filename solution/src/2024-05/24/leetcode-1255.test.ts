026c7a9e7713f49f57880e9baaad7e59f747fecb
sangrimlee <contact@sangrim.dev>
2024-05-24 20:48:21 +0900

    :pencil2: 2024-05-24
    - LeetCode 1255. Maximum Score Words Formed by Letters
    - https://leetcode.com/problems/maximum-score-words-formed-by-letters

---
+++ solution/src/2024-05/24/leetcode-1255.test.ts
@@ -0,0 +1,31 @@
+import { maxScoreWords } from './leetcode-1255';
+
+describe('2024-05-24: LeetCode 1255', () => {
+  test('Example 1', () => {
+    expect(
+      maxScoreWords(
+        ['dog', 'cat', 'dad', 'good'],
+        ['a', 'a', 'c', 'd', 'd', 'd', 'g', 'o', 'o'],
+        [1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ),
+    ).toBe(23);
+  });
+  test('Example 2', () => {
+    expect(
+      maxScoreWords(
+        ['xxxz', 'ax', 'bx', 'cx'],
+        ['z', 'a', 'b', 'c', 'x', 'x', 'x'],
+        [4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10],
+      ),
+    ).toBe(27);
+  });
+  test('Example 3', () => {
+    expect(
+      maxScoreWords(
+        ['leetcode'],
+        ['l', 'e', 't', 'c', 'o', 'd'],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+      ),
+    ).toBe(0);
+  });
+});
