e15eb8237ab7dd883365dd7762898d3ae130db57
sangrimlee <contact@sangrim.dev>
2024-06-01 21:09:38 +0900

    :pencil2: 2024-06-01
    - LeetCode 890. Find and Replace Pattern
    - https://leetcode.com/problems/find-and-replace-pattern

---
+++ solution/src/2024-06/01/leetcode-890.ts
@@ -0,0 +1,31 @@
+/**
+ * 890. Find and Replace Pattern
+ * https://leetcode.com/problems/find-and-replace-pattern
+ */
+export function findAndReplacePattern(words: string[], pattern: string): string[] {
+  const p = normalize(pattern);
+  return words.filter((word) => isEqual(p, normalize(word)));
+}
+
+function normalize(word: string): number[] {
+  const result = [];
+  const map = new Map<string, number>();
+  for (let i = 0; i < word.length; i++) {
+    const id = map.get(word[i]) ?? map.size;
+    map.set(word[i], id);
+    result.push(id);
+  }
+  return result;
+}
+
+function isEqual<T>(arr1: T[], arr2: T[]): boolean {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
