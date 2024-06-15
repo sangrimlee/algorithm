fbfbb15558106385d7625c6066c61feeaa82967f
sangrimlee <contact@sangrim.dev>
2024-06-15 11:44:23 +0900

    test: 1104번 테스트 코드 수정
    - 잘못된 테스트 코드 수정

--- solution/src/2024-06/13/leetcode-1104.test.ts
+++ solution/src/2024-06/13/leetcode-1104.test.ts
@@ -1,8 +1,10 @@
+import { pathInZigZagTree } from './leetcode-1104';
+
 describe('2024-06-13: LeetCode 1104', () => {
   test('Example 1', () => {
-    expect(solution(14)).toEqual([1, 3, 4, 14]);
+    expect(pathInZigZagTree(14)).toEqual([1, 3, 4, 14]);
   });
   test('Example 2', () => {
-    expect(solution(26)).toEqual([1, 2, 6, 10, 26]);
+    expect(pathInZigZagTree(26)).toEqual([1, 2, 6, 10, 26]);
   });
 });
