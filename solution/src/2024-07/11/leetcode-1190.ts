8582afb9a350ca34c696cb23d6adc54ea8cb0821
sangrimlee <contact@sangrim.dev>
2024-07-11 21:54:50 +0900

    :pencil2: 2024-07-11
    - LeetCode 1190. Reverse Substrings Between Each Pair of Parentheses
    - https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses

---
+++ solution/src/2024-07/11/leetcode-1190.ts
@@ -0,0 +1,23 @@
+/**
+ * 1190. Reverse Substrings Between Each Pair of Parentheses
+ * https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses
+ */
+export function reverseParentheses(s: string): string {
+  const stack = [''];
+  for (const char of s) {
+    if (char === '(') {
+      stack.push('');
+    } else if (char === ')') {
+      const prev = stack.pop()!;
+      stack[stack.length - 1] += reverseString(prev);
+    } else {
+      stack[stack.length - 1] += char;
+    }
+  }
+
+  return stack.join('');
+}
+
+function reverseString(s: string) {
+  return [...s].reverse().join('');
+}
