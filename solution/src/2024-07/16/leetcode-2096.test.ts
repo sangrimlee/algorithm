a0f19be9f5ac9609a5992b1c6d060f8fa6d99e9d
sangrimlee <contact@sangrim.dev>
2024-07-16 22:35:09 +0900

    :pencil2: 2024-07-16
    - LeetCode 2096. Step-By-Step Directions From a Binary Tree Node to Another
    - https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another

---
+++ solution/src/2024-07/16/leetcode-2096.test.ts
@@ -0,0 +1,11 @@
+import { createTree } from '@algorithm/lib';
+import { getDirections } from './leetcode-2096';
+
+describe('2024-07-16: LeetCode 2096', () => {
+  test('Example 1', () => {
+    expect(getDirections(createTree([5, 1, 2, 3, null, 6, 4]), 3, 6)).toEqual('UURL');
+  });
+  test('Example 2', () => {
+    expect(getDirections(createTree([2, 1]), 2, 1)).toEqual('L');
+  });
+});
---
+++ solution/src/2024-07/16/leetcode-2096.ts
@@ -0,0 +1,55 @@
+import { TreeNode } from '@algorithm/lib';
+
+/**
+ * 2096. Step-By-Step Directions From a Binary Tree Node to Another
+ * https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another
+ */
+export function getDirections(
+  root: TreeNode | null,
+  startValue: number,
+  destValue: number,
+): string {
+  const lcaNode = getLCA(root, startValue, destValue);
+  const startPath = getPath(lcaNode, startValue, []);
+  const destPath = getPath(lcaNode, destValue, []);
+  return 'U'.repeat(startPath.length) + destPath;
+}
+
+function getLCA(node: TreeNode | null, startValue: number, destValue: number): TreeNode | null {
+  if (!node) {
+    return null;
+  }
+  if (node.val === startValue || node.val === destValue) {
+    return node;
+  }
+  const leftNode = getLCA(node.left, startValue, destValue);
+  const rightNode = getLCA(node.right, startValue, destValue);
+  if (leftNode && rightNode) {
+    return node;
+  }
+  return leftNode ? leftNode : rightNode;
+}
+
+function getPath(node: TreeNode | null, value: number, paths: string[]): string {
+  if (!node) {
+    return '';
+  }
+  if (node.val === value) {
+    return paths.join('');
+  }
+
+  paths.push('L');
+  const leftPath = getPath(node.left, value, paths);
+  if (leftPath !== '') {
+    return leftPath;
+  }
+  paths.pop();
+
+  paths.push('R');
+  const rightPath = getPath(node.right, value, paths);
+  if (rightPath !== '') {
+    return rightPath;
+  }
+  paths.pop();
+  return '';
+}
