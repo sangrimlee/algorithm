588cfdfbd82814c749e622881b0580b83c121b97
sangrimlee <contact@sangrim.dev>
2024-07-30 22:15:21 +0900

    :pencil2: 2024-07-29
    - LeetCode 1395. Count Number of Teams
    - https://leetcode.com/problems/count-number-of-teams

---
+++ solution/src/2024-07/29/leetcode-1395.test.ts
@@ -0,0 +1,13 @@
+import { numTeams } from './leetcode-1395';
+
+describe('2024-07-29: LeetCode 1395', () => {
+  test('Example 1', () => {
+    expect(numTeams([2, 5, 3, 4, 1])).toBe(3);
+  });
+  test('Example 2', () => {
+    expect(numTeams([2, 1, 3])).toBe(0);
+  });
+  test('Example 3', () => {
+    expect(numTeams([1, 2, 3, 4])).toBe(4);
+  });
+});
