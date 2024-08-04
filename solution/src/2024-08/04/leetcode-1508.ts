faca15e05f594cc6d2f103ca17790e329ae40b24
sangrimlee <contact@sangrim.dev>
2024-08-04 10:52:34 +0900

    :pencil2: 2024-08-04
    - LeetCode 1508. Range Sum of Sorted Subarray Sums
    - https://leetcode.com/problems/range-sum-of-sorted-subarray-sums

---
+++ solution/src/2024-08/04/leetcode-1508.ts
@@ -0,0 +1,25 @@
+import { Heap } from '@algorithm/lib';
+
+/**
+ * 1508. Range Sum of Sorted Subarray Sums
+ * https://leetcode.com/problems/range-sum-of-sorted-subarray-sums
+ */
+export function rangeSum(nums: number[], n: number, left: number, right: number): number {
+  const MOD = 1_000_000_007;
+  const heap = new Heap<[number, number]>((a, b) => (a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]));
+  for (let i = 0; i < n; i++) {
+    heap.push([nums[i], i]);
+  }
+
+  let answer = 0;
+  for (let mid = 1; mid <= right; mid++) {
+    const [num, i] = heap.pop()!;
+    if (left <= mid) {
+      answer += num;
+    }
+    if (i + 1 < n) {
+      heap.push([num + nums[i + 1], i + 1]);
+    }
+  }
+  return answer % MOD;
+}
