d19a0c8f31be966687433666ef1ae0396a140e43
sangrimlee <contact@sangrim.dev>
2024-08-23 21:59:17 +0900

    :pencil2: 2024-08-23
    - LeetCode 592. Fraction Addition and Subtraction
    - https://leetcode.com/problems/fraction-addition-and-subtraction

---
+++ solution/src/2024-08/23/leetcode-592.ts
@@ -0,0 +1,26 @@
+/**
+ * 592. Fraction Addition and Subtraction
+ * https://leetcode.com/problems/fraction-addition-and-subtraction
+ */
+export function fractionAddition(expression: string): string {
+  const nums = [...expression.matchAll(/[+-]?\d+/g)].map((v) => +v);
+
+  let [numerator, denominator] = [0, 1];
+  for (let i = 0; i < nums.length; i += 2) {
+    const [num, den] = [nums[i], nums[i + 1]];
+    numerator = numerator * den + denominator * num;
+    denominator *= den;
+
+    const gcdValue = gcd(Math.abs(numerator), denominator);
+    numerator /= gcdValue;
+    denominator /= gcdValue;
+  }
+  return `${numerator}/${denominator}`;
+}
+
+function gcd(a: number, b: number): number {
+  while (0 < b) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
