7d6cedeebf33bf1811e934f828f9711ecc782797
sangrimlee <contact@sangrim.dev>
2024-10-10 11:35:06 +0900

    :pencil2: 2024-10-10
    - LeetCode 962. Maximum Width Ramp
    - https://leetcode.com/problems/maximum-width-ramp

---
+++ solution/src/2024-10/10/leetcode-962.ts
@@ -0,0 +1,21 @@
+/**
+ * 962. Maximum Width Ramp
+ * https://leetcode.com/problems/maximum-width-ramp
+ */
+export function maxWidthRamp(nums: number[]): number {
+  const n = nums.length;
+  const stack: number[] = [];
+  for (let i = 0; i < n; i++) {
+    if (stack.length === 0 || nums[i] < nums[stack[stack.length - 1]]) {
+      stack.push(i);
+    }
+  }
+
+  let answer = 0;
+  for (let i = n - 1; 0 <= i; i--) {
+    while (0 < stack.length && nums[stack[stack.length - 1]] <= nums[i]) {
+      answer = Math.max(answer, i - stack.pop()!);
+    }
+  }
+  return answer;
+}
