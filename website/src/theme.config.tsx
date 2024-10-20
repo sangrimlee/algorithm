2bc31aa86af5debd8b37e7cabcbf4e6177cde827
sangrimlee <contact@sangrim.dev>
2024-10-20 21:58:36 +0900

    feat: `nextra` 관련 설정 수정
    - `mathjax` 사용 가능하도록 수정
    - `codeblocks`는 검색하지않도록 변경
    - `title` 관련 설정 수정

--- website/src/theme.config.tsx
+++ website/src/theme.config.tsx
@@ -15,13 +15,13 @@ const config: DocsThemeConfig = {
   logo: <Logo />,
   head: function useHead() {
     const { asPath } = useRouter();
-    const { title, frontMatter } = useConfig();
+    const { frontMatter } = useConfig();
 
     const baseURL = new URL(env.NEXT_PUBLIC_URL);
     const domain = baseURL.hostname;
     const ogImage = new URL('/images/og.png', baseURL).href;
     const url = new URL(asPath, baseURL).href;
-
+    const title = frontMatter.title ? `${frontMatter.title} | Algorithm` : 'Algorithm';
     const description =
       frontMatter.description ??
       'LeetCode와 Programmers의 문제들을 TypeScript를 사용한 문제 풀이 저장소';
@@ -37,13 +37,13 @@ const config: DocsThemeConfig = {
 
     return (
       <>
-        <title>{title ? `${title} | Algorithm` : 'Algorithm'}</title>
+        <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <meta name="author" content="sangrimlee" />
         <meta name="description" content={description} />
         <meta name="keywords" content={tags.join(', ')} />
-        <meta name="og:title" content={title ? `${title} | Algorithm` : 'Algorithm'} />
+        <meta name="og:title" content={title} />
         <meta name="og:description" content={description} />
         <meta name="og:image" content={ogImage} />
         <meta property="og:image:width" content="1200" />
@@ -53,7 +53,7 @@ const config: DocsThemeConfig = {
         <meta property="og:locale" content="ko_KR" />
         <meta property="og:url" content={url} />
 
-        <meta property="twitter:title" content={title ? title + ' |  Algorithm' : 'Algorithm'} />
+        <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImage} />
@@ -73,7 +73,7 @@ const config: DocsThemeConfig = {
     content: <Footer />,
   },
   search: {
-    placeholder: 'Search',
+    placeholder: '검색',
   },
   components: {
     Badges,
