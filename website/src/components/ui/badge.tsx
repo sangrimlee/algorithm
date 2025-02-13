4512d7c567ac39a047a95ab33b2ca70b65b2afed
이상림 <contact@sangrim.dev>
2025-02-13 23:15:38 +0900

    chore(format): 잘못된 포맷팅 수정
    - `prettier`에 맞지 않는 잘못된 포맷팅 수정

--- website/src/components/ui/badge.tsx
+++ website/src/components/ui/badge.tsx
@@ -6,27 +6,27 @@ import { Slot } from '@radix-ui/react-slot';
 
 const badgeVariants = cva(
   [
-    'inline-flex items-center rounded-full ring-1 ring-current px-2.5 py-1.5 text-sm leading-none font-medium transition-colors',
-    'focus-visible:outline-none focus-visible:shadow-focus-ring',
+    'inline-flex items-center rounded-full px-2.5 py-1.5 text-sm leading-none font-medium ring-1 ring-current transition-colors',
+    'focus-visible:shadow-focus-ring focus-visible:outline-none',
   ],
   {
     variants: {
       variant: {
         blue: [
-          'text-blue-700 bg-blue-700/10 hover:bg-blue-700/20',
-          'dark:text-blue-300 dark:bg-blue-300/10 hover:dark:bg-blue-300/20',
+          'bg-blue-700/10 text-blue-700 hover:bg-blue-700/20',
+          'dark:bg-blue-300/10 dark:text-blue-300 hover:dark:bg-blue-300/20',
         ],
         green: [
-          'text-green-700 bg-green-700/10 hover:bg-green-700/20',
-          'dark:text-green-300 dark:bg-green-300/10 hover:dark:bg-green-300/20',
+          'bg-green-700/10 text-green-700 hover:bg-green-700/20',
+          'dark:bg-green-300/10 dark:text-green-300 hover:dark:bg-green-300/20',
         ],
         yellow: [
-          'text-yellow-700 bg-yellow-700/10 hover:bg-yellow-700/20',
-          'dark:text-yellow-300 dark:bg-yellow-300/10 hover:dark:bg-yellow-300/20',
+          'bg-yellow-700/10 text-yellow-700 hover:bg-yellow-700/20',
+          'dark:bg-yellow-300/10 dark:text-yellow-300 hover:dark:bg-yellow-300/20',
         ],
         red: [
-          'text-red-700 bg-red-700/10 hover:bg-red-600/20',
-          'dark:text-red-300 dark:bg-red-300/10 hover:dark:bg-red-300/20',
+          'bg-red-700/10 text-red-700 hover:bg-red-600/20',
+          'dark:bg-red-300/10 dark:text-red-300 hover:dark:bg-red-300/20',
         ],
       },
     },
