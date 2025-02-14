48af387f0f7a49a3604a4d0e034fd121c5ecf247
이상림 <contact@sangrim.dev>
2025-02-14 13:49:23 +0900

    chore: `eslint` 설정 수정
    - `website`의 `eslint` 설정에서 빌드 결과물인 `.next`를 제외

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,3 +1,3 @@
 import { configs } from '@algorithm/eslint-config';
 
-export default [...configs.base, ...configs.next, configs.prettier];
+export default [{ ignores: ['.next/**'] }, ...configs.base, ...configs.next, configs.prettier];
