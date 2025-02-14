67eba953ee946426bdd64bc4b42b358150a14edc
이상림 <contact@sangrim.dev>
2025-02-14 13:54:56 +0900

    feat: `eslint-plugin-import` 규칙 수정
    - `typescript-eslint`에서 제공하는 규칙과 동일한 검사를 제공하는 규칙을 사용하지 않음으로 변경

--- tooling/eslint/src/base.js
+++ tooling/eslint/src/base.js
@@ -10,9 +10,22 @@ export default tseslint.config(
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
   turboPlugin.configs['flat/recommended'],
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
+    rules: {
+      /**
+       * `typescript-eslint`가 동일한 검사를 제공하므로, 다음 규칙을 사용하지 않음
+       * https://typescript-eslint.io/troubleshooting/typed-linting/performance/#eslint-plugin-import
+       */
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+    },
+  },
   {
     linterOptions: { reportUnusedDisableDirectives: true },
     languageOptions: {
