f4cc871625355ab7a527348ed4a362ec56f6d5bf
이상림 <contact@sangrim.dev>
2025-02-15 15:34:08 +0900

    chore(lint): `eslint` 규칙에 맞게 수정
    - `website`의 코드를 `eslint` 규칙에 맞게 수정

--- website/src/components/logo/blue-sky.tsx
+++ website/src/components/logo/blue-sky.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 
 export const BlueSkyLogo = (props: SVGProps<SVGSVGElement>) => {
   return (
--- website/src/components/logo/x.tsx
+++ website/src/components/logo/x.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 
 export const XLogo = (props: SVGProps<SVGSVGElement>) => {
   return (
