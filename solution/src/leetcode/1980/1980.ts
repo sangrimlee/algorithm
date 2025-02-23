5d6b1205aca284cb936a28a0f3874f1467bbe84f
이상림 <contact@sangrim.dev>
2025-02-24 00:15:15 +0900

    :pencil2: Find Unique Binary String

--- solution/src/leetcode/1980/1980.ts
+++ solution/src/leetcode/1980/1980.ts
@@ -3,6 +3,11 @@
  * https://leetcode.com/problems/find-unique-binary-string
  */
 export function findDifferentBinaryString(nums: string[]): string {
-  const not = (bit: string) => (bit === '0' ? '1' : '0');
-  return nums.reduce((answer, num, i) => answer + not(num[i]), '');
+  const n = nums.length;
+  const answer: string[] = [];
+  for (let i = 0; i < n; i++) {
+    const curr = nums[i][i]; // i번째 문자열의 i번째 비트 선택
+    answer.push(curr === '0' ? '1' : '0'); // 비트를 반전
+  }
+  return answer.join('');
 }
