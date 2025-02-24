1bcfba3b66e9f0d8aa2a51f27ceaf228047dbca3
이상림 <contact@sangrim.dev>
2025-02-24 22:40:23 +0900

    feat: `styled-jsx`로 변경
    - 복잡한 css의 경우, `styled-jsx`로 변경

--- website/src/app/_components/background.tsx
+++ website/src/app/_components/background.tsx
@@ -17,16 +17,47 @@ const variants = {
 
 export const Background = () => {
   return (
-    <div className="absolute inset-0 -z-10">
-      <div className="bg-grid absolute inset-0" />
-      <motion.div initial="hidden" animate="show" variants={variants}>
-        <div
-          className={cx(
-            'bg-gradient absolute inset-1/2 h-48 w-48 -translate-x-1/2 -translate-y-1/2 animate-pulse opacity-30 blur-3xl',
-            'md:h-96 md:w-96 md:blur-[128px] xl:h-[32rem] xl:w-[32rem]',
-          )}
-        />
-      </motion.div>
-    </div>
+    <>
+      <div className="absolute inset-0 -z-10">
+        <div className={cx('bg-grid', 'absolute inset-0')} />
+        <motion.div initial="hidden" animate="show" variants={variants}>
+          <div
+            className={cx(
+              'bg-gradient',
+              'absolute inset-1/2 h-48 w-48 -translate-x-1/2 -translate-y-1/2 animate-pulse opacity-30 blur-3xl',
+              'md:h-96 md:w-96 md:blur-[128px] xl:h-[32rem] xl:w-[32rem]',
+            )}
+          />
+        </motion.div>
+      </div>
+      <style jsx>
+        {`
+          .bg-grid {
+            background-color: transparent;
+            background-size: 4rem 4rem;
+            background-image:
+              linear-gradient(
+                color-mix(in oklab, var(--color-gray-4) 5%, transparent) 1px,
+                transparent 1px
+              ),
+              linear-gradient(
+                to right,
+                color-mix(in oklab, var(--color-gray-4) 5%, transparent) 1px,
+                transparent 1px
+              );
+          }
+          .bg-gradient {
+            background-image: conic-gradient(
+              from 0deg at 50% 50%,
+              var(--red9) 0deg,
+              var(--amber9) 120deg,
+              var(--blue9) 240deg,
+              var(--red9) 360deg,
+              var(--amber9) 480deg
+            );
+          }
+        `}
+      </style>
+    </>
   );
 };
--- website/src/components/logo/logo.tsx
+++ website/src/components/logo/logo.tsx
@@ -1,7 +1,24 @@
 'use client';
 
+import { cx } from '@/utils/cx';
+
 export const Logo = () => {
   return (
-    <div className="logo-animation font-logo text-xl font-extrabold tracking-tight">Algorithm</div>
+    <>
+      <div className={cx('logo-animation', 'font-logo text-xl font-extrabold tracking-tight')}>
+        Algorithm
+      </div>
+      <style jsx>{`
+        .logo-animation {
+          mask-image: linear-gradient(60deg, black 25%, rgba(0, 0, 0, 0.25) 50%, black 75%);
+          mask-size: 400%;
+          mask-position: 0%;
+          &:hover {
+            mask-position: 100%;
+            transition: mask-position 1s ease;
+          }
+        }
+      `}</style>
+    </>
   );
 };
