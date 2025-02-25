bb1901639a63a7546dc5f6a6d7930c37cf8284d9
이상림 <contact@sangrim.dev>
2025-02-25 14:11:53 +0900

    feat: `Badge` 컴포넌트 수정
    - `Badge` 컴포넌트 수정
    - `size` 추가 및 `variant` 종류 추가

--- website/src/components/ui/badge.tsx
+++ website/src/components/ui/badge.tsx
@@ -7,32 +7,39 @@ import { Slot } from '@radix-ui/react-slot';
 
 const badgeVariants = cva(
   [
-    'inline-flex items-center rounded-full px-2.5 py-1.5 text-sm leading-none font-medium ring-1 ring-current transition-colors',
+    'inline-flex shrink-0 items-center justify-center rounded-full font-medium whitespace-nowrap capitalize',
     'focus-visible:shadow-focus-ring focus-visible:outline-none',
   ],
   {
     variants: {
+      size: {
+        sm: 'h-5 gap-0.5 px-1.5 text-[11px] [&_svg]:size-3',
+        md: 'h-6 gap-1 px-2.5 text-xs [&_svg]:size-3.5',
+        lg: 'h-8 gap-1.5 px-3 text-sm [&_svg]:size-4',
+      },
       variant: {
-        blue: [
-          'bg-blue-700/10 text-blue-700 hover:bg-blue-700/20',
-          'dark:bg-blue-300/10 dark:text-blue-300 hover:dark:bg-blue-300/20',
-        ],
-        green: [
-          'bg-green-700/10 text-green-700 hover:bg-green-700/20',
-          'dark:bg-green-300/10 dark:text-green-300 hover:dark:bg-green-300/20',
-        ],
-        yellow: [
-          'bg-yellow-700/10 text-yellow-700 hover:bg-yellow-700/20',
-          'dark:bg-yellow-300/10 dark:text-yellow-300 hover:dark:bg-yellow-300/20',
-        ],
-        red: [
-          'bg-red-700/10 text-red-700 hover:bg-red-600/20',
-          'dark:bg-red-300/10 dark:text-red-300 hover:dark:bg-red-300/20',
-        ],
+        gray: 'bg-gray-9 text-white',
+        blue: 'bg-blue-9 text-white',
+        purple: 'bg-purple-9 text-white',
+        amber: 'bg-amber-9 text-black',
+        red: 'bg-red-9 text-white',
+        pink: 'bg-pink-9 text-white',
+        green: 'bg-green-9 text-white',
+        teal: 'bg-teal-9 text-white',
+        'gray-subtle': 'bg-gray-3 text-gray-12',
+        'blue-subtle': 'bg-blue-3 text-blue-11',
+        'purple-subtle': 'bg-purple-3 text-purple-11',
+        'amber-subtle': 'bg-amber-3 text-amber-11',
+        'red-subtle': 'bg-red-3 text-red-11',
+        'pink-subtle': 'bg-pink-3 text-pink-11',
+        'green-subtle': 'bg-green-3 text-green-11',
+        'teal-subtle': 'bg-teal-3 text-teal-11',
+        inverted: 'bg-gray-12 text-gray-1',
       },
     },
     defaultVariants: {
-      variant: 'blue',
+      size: 'md',
+      variant: 'gray',
     },
   },
 );
@@ -44,10 +51,14 @@ export interface BadgeProps
 }
 
 export const Badge = React.forwardRef<HTMLSpanElement, BadgeProps>(
-  ({ className, variant, asChild = false, ...props }, forwardedRef) => {
+  ({ className, variant, size, asChild = false, ...props }, forwardedRef) => {
     const Comp = asChild ? Slot : 'span';
     return (
-      <Comp className={cn(badgeVariants({ variant }), className)} ref={forwardedRef} {...props} />
+      <Comp
+        className={cn(badgeVariants({ variant, size }), className)}
+        ref={forwardedRef}
+        {...props}
+      />
     );
   },
 );
