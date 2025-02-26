2bf34abfaa8ef5f29c6355c87134f2dea39d15d4
이상림 <contact@sangrim.dev>
2025-02-26 14:33:08 +0900

    feat: `MDX` 컴포넌트 수정
    - `remark`, `rehype` 플러그인 추가

--- website/src/components/mdx/mdx.tsx
+++ website/src/components/mdx/mdx.tsx
@@ -1,12 +1,30 @@
 import type { MDXRemoteProps } from 'next-mdx-remote/rsc';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeSlug from 'rehype-slug';
+import rehypeKatex from 'rehype-katex';
+import { rehypePrettyCode } from 'rehype-pretty-code';
+import type { Options as RehypePrettyCodeOptions } from 'rehype-pretty-code';
+
 import { mdxComponents } from './mdx-components';
+
+const rehypePrettyCodeOptions = {
+  grid: true,
+  keepBackground: false,
+  bypassInlineCode: true,
+  defaultLang: 'plaintext',
+  theme: {
+    dark: 'github-dark',
+    light: 'github-light',
+  },
+} satisfies RehypePrettyCodeOptions;
 
 const mdxOptions = {
   mdxOptions: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeSlug, rehypeKatex, [rehypePrettyCode, rehypePrettyCodeOptions]],
     format: 'mdx',
   },
 } satisfies MDXRemoteProps['options'];
