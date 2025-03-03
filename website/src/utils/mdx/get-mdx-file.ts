ee908f5f6967de9d2465aba6ad44c62956479731
이상림 <contact@sangrim.dev>
2025-03-03 17:45:21 +0900

    feat: `getMDXFile` 수정
    - `mdx` 파일에서 `githubIssueLink` 및 `githubLink` 메타 데이터 추가

--- website/src/utils/mdx/get-mdx-file.ts
+++ website/src/utils/mdx/get-mdx-file.ts
@@ -2,11 +2,17 @@ import matter from 'gray-matter';
 import type { z } from 'zod';
 
 import { exists, readFile } from '../fs';
+import { getGithubIssueLink, getGithubLink } from '../github';
 
-export async function getMDXFile<T>(
+type MDXFileMetadta<T extends { title: string }> = T & {
+  githubLink: string;
+  githubIssueLink: string;
+};
+
+export async function getMDXFile<T extends { title: string }>(
   filePath: string,
   schema: z.Schema<T>,
-): Promise<{ source: string; metadata: T } | null> {
+): Promise<{ source: string; metadata: MDXFileMetadta<T> } | null> {
   const isExist = await exists(filePath);
   if (!isExist) {
     return null;
@@ -14,5 +20,12 @@ export async function getMDXFile<T>(
   const mdxSource = await readFile(filePath);
   const { content: source, data } = matter(mdxSource);
   const metadata = await schema.parseAsync(data);
-  return { source, metadata };
+  return {
+    source,
+    metadata: {
+      ...metadata,
+      githubLink: getGithubLink(filePath),
+      githubIssueLink: getGithubIssueLink(metadata.title),
+    },
+  };
 }
