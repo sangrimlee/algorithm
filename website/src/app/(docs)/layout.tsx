12da6de1cc7878e482e2adafb5b1fb5ec5741562
이상림 <contact@sangrim.dev>
2025-03-03 17:58:29 +0900

    feat: `DocsLayout` 스타일 수정
    - `container` 적용

--- website/src/app/(docs)/layout.tsx
+++ website/src/app/(docs)/layout.tsx
@@ -1,5 +1,3 @@
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mx-auto flex w-full max-w-(--content-width) items-start gap-12">{children}</div>
-  );
+  return <div className="container flex items-start gap-12">{children}</div>;
 }
