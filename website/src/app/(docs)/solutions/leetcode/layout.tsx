453aa5d8ebacea514d5354d30748abeb934ad419
이상림 <contact@sangrim.dev>
2025-03-04 15:12:33 +0900

    feat: `LeetCode` 페이지 레이아웃 추가

---
+++ website/src/app/(docs)/solutions/leetcode/layout.tsx
@@ -0,0 +1,26 @@
+import * as Sidebar from '@/components/ui/sidebar';
+import { meta } from './_meta';
+
+export default function LeetCodeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Sidebar.Root className="pt-5 max-lg:hidden">
+        <Sidebar.Content>
+          <Sidebar.List>
+            <Sidebar.Item>
+              <Sidebar.Link type="label" href="/solutions/leetcode">
+                문제 목록
+              </Sidebar.Link>
+            </Sidebar.Item>
+            {Object.entries(meta).map(([slug, name]) => (
+              <Sidebar.Item key={slug}>
+                <Sidebar.Link href={`/solutions/leetcode/${slug}`}>{name}</Sidebar.Link>
+              </Sidebar.Item>
+            ))}
+          </Sidebar.List>
+        </Sidebar.Content>
+      </Sidebar.Root>
+      {children}
+    </>
+  );
+}
