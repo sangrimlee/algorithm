28cf0cb06415f47601b711c896f1532deed1159a
이상림 <contact@sangrim.dev>
2025-03-06 22:45:09 +0900

    feat: `LeetCode` 문제 목록 페이지 추가

--- website/src/app/(docs)/solutions/leetcode/page.tsx
+++ website/src/app/(docs)/solutions/leetcode/page.tsx
@@ -1,7 +1,17 @@
+import * as Breadcrumb from '@/components/ui/breadcrumb';
+
 export default function LeetCodePage() {
   return (
-    <>
-      <h1>LeetCode</h1>
-    </>
+    <article className="min-h-(--content-height) w-full min-w-0 pt-4 pb-16">
+      <Breadcrumb.Root className="mt-2">
+        <Breadcrumb.List>
+          <Breadcrumb.Page>LeetCode</Breadcrumb.Page>
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+      <div className="mt-4 mb-8">
+        <h1 className="text-4xl font-semibold tracking-tight text-gray-12">문제 목록</h1>
+      </div>
+      <div></div>
+    </article>
   );
 }
