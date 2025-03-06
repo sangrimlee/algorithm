be474f0e8b561d0938c3997d8ad870023a5301b6
이상림 <contact@sangrim.dev>
2025-03-06 22:45:42 +0900

    feat: `Programmers` 문제 목록 페이지 추가

--- website/src/app/(docs)/solutions/programmers/page.tsx
+++ website/src/app/(docs)/solutions/programmers/page.tsx
@@ -1,7 +1,17 @@
+import * as Breadcrumb from '@/components/ui/breadcrumb';
+
 export default function ProgrammersPage() {
   return (
-    <>
-      <h1>Programmers</h1>
-    </>
+    <article className="min-h-(--content-height) w-full min-w-0 pt-4 pb-16">
+      <Breadcrumb.Root className="mt-2">
+        <Breadcrumb.List>
+          <Breadcrumb.Page>Programmers</Breadcrumb.Page>
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+      <div className="mt-4 mb-8">
+        <h1 className="text-4xl font-semibold tracking-tight text-gray-12">문제 목록</h1>
+      </div>
+      <div></div>
+    </article>
   );
 }
