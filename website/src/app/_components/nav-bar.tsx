839b78e172d94525b80e585fc3fe9fb1d8bc627b
이상림 <contact@sangrim.dev>
2025-03-06 21:02:59 +0900

    feat: `NavBar` 수정
    - 기본적인 `NavBar` 추가

--- website/src/app/_components/nav-bar.tsx
+++ website/src/app/_components/nav-bar.tsx
@@ -1,11 +1,44 @@
+'use client';
+
+import { useMemo } from 'react';
 import Link from 'next/link';
+
 import { MenuIcon } from 'lucide-react';
 
 import { GithubLogo, Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
+import { cx } from '@/utils/cx';
 import { ThemeToggle } from './theme-toggle';
+import { usePathname } from 'next/navigation';
 
-export const NavBar = () => {
+type NavLinkProps = React.ComponentPropsWithoutRef<typeof Link>;
+function NavLink({ href, className, children, ...props }: NavLinkProps) {
+  const pathname = usePathname();
+
+  const isActive = useMemo(() => {
+    if (typeof href === 'string') {
+      return pathname.startsWith(href);
+    }
+    return pathname.startsWith(href.href ?? href.pathname ?? '');
+  }, [href, pathname]);
+
+  return (
+    <Link
+      href={href}
+      className={cx(
+        'rounded-md px-2 py-1 text-sm font-medium transition-colors',
+        'hover:text-gray-12 focus-visible:shadow-focus-ring focus-visible:outline-none',
+        isActive ? 'font-semibold text-gray-12' : 'text-gray-11',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export function NavBar() {
   return (
     <header className="sticky inset-x-0 top-0 z-sticky border-b border-dashed border-gray-6 bg-gray-1/90 backdrop-blur-sm">
       <div className="container flex h-(--navbar-height) items-center justify-between">
@@ -13,7 +46,14 @@ export const NavBar = () => {
           <Link href="/">
             <Logo />
           </Link>
-          {/* TODO: 링크 리스트 추가 */}
+          <ul className="ml-8 hidden items-center gap-x-2 sm:flex">
+            <li className="flex">
+              <NavLink href="/solutions/leetcode">LeetCode</NavLink>
+            </li>
+            <li className="flex">
+              <NavLink href="/solutions/programmers">Programmers</NavLink>
+            </li>
+          </ul>
         </nav>
         <div className="flex items-center sm:hidden">
           {/* TODO: 모바일 메뉴 추가 */}
@@ -39,4 +79,4 @@ export const NavBar = () => {
       </div>
     </header>
   );
-};
+}
