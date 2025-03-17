2d04a10407773558ab665f07cea3778c7a5115ab
이상림 <contact@sangrim.dev>
2025-03-17 11:26:39 +0900

    :pencil2: Divide Array Into Equal Pairs
    - LeetCode 2206. Divide Array Into Equal Pairs 문제 풀이 수정 및 페이지 수정

--- solution/src/leetcode/2206/2206.ts
+++ solution/src/leetcode/2206/2206.ts
@@ -3,9 +3,13 @@
  * https://leetcode.com/problems/divide-array-into-equal-pairs
  */
 export function divideArray(nums: number[]): boolean {
-  const counter = new Map<number, number>();
-  nums.forEach((num) => {
-    counter.set(num, (counter.get(num) ?? 0) + 1);
-  });
-  return [...counter.values()].every((count) => count % 2 === 0);
+  const set = new Set<number>();
+  for (const num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+  return set.size === 0;
 }
