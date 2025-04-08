003e603dbb428ab0dea2da51710900805cf2c9fe
이상림 <contact@sangrim.dev>
2025-04-08 17:10:40 +0900

    :pencil2: Largest Divisible Subset
    - Largest Divisible Subset 문제 풀이 및 해설 페이지 수정

--- solution/src/leetcode/0368/0368.ts
+++ solution/src/leetcode/0368/0368.ts
@@ -4,30 +4,29 @@
  */
 export function largestDivisibleSubset(nums: number[]): number[] {
   nums.sort((a, b) => a - b);
+
   const n = nums.length;
   const dp = new Array<number>(n).fill(1);
+  const prev = new Array<number>(n).fill(-1);
 
-  let maxSize = 1;
   let maxIndex = 0;
-  for (let i = 0; i < n; i++) {
+  for (let i = 1; i < n; i++) {
     for (let j = 0; j < i; j++) {
-      if (nums[i] % nums[j] !== 0) continue;
-      dp[i] = Math.max(dp[i], dp[j] + 1);
-      if (maxSize < dp[i]) {
-        maxSize = dp[i];
-        maxIndex = i;
+      if (nums[i] % nums[j] === 0 && dp[i] < dp[j] + 1) {
+        dp[i] = dp[j] + 1;
+        prev[i] = j;
       }
+    }
+    if (dp[maxIndex] < dp[i]) {
+      maxIndex = i;
     }
   }
 
-  const answer = [];
-  let maxNum = nums[maxIndex];
-  for (let i = maxIndex; 0 <= i; i--) {
-    if (maxNum % nums[i] === 0 && dp[i] === maxSize) {
-      answer.push(nums[i]);
-      maxNum = nums[i];
-      maxSize -= 1;
-    }
+  const answer: number[] = [];
+  let currentIndex = maxIndex;
+  while (currentIndex !== -1) {
+    answer.push(nums[currentIndex]);
+    currentIndex = prev[currentIndex];
   }
   return answer;
 }
