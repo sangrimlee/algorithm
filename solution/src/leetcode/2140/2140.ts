1de2cd15934dfe051c18e9a4442a0f84eac3196e
이상림 <contact@sangrim.dev>
2025-04-08 16:11:39 +0900

    :pencil2: Solving Questions With Brainpower
    - Solving Questions With Brainpower 문제 풀이와 해설 페이지 수정

--- solution/src/leetcode/2140/2140.ts
+++ solution/src/leetcode/2140/2140.ts
@@ -4,19 +4,11 @@
  */
 export function mostPoints(questions: number[][]): number {
   const n = questions.length;
-  const dp = new Array<number>(n).fill(0);
+  const dp = new Array<number>(n + 1).fill(0);
 
-  const dfs = (i: number) => {
-    if (n <= i) {
-      return 0;
-    }
-    if (dp[i] !== 0) {
-      return dp[i];
-    }
-    const [point, brainPower] = questions[i];
-    dp[i] = Math.max(dfs(i + 1), point + dfs(i + brainPower + 1));
-    return dp[i];
-  };
-
-  return dfs(0);
+  for (let i = n - 1; i >= 0; i--) {
+    const [points, brainpower] = questions[i];
+    dp[i] = Math.max(points + dp[Math.min(n, brainpower + i + 1)], dp[i + 1]);
+  }
+  return dp[0];
 }
