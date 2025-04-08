f36cc7e73997773b33ac336d24d391e58c617939
이상림 <contact@sangrim.dev>
2025-04-08 16:15:33 +0900

    :pencil2: Maximum Value of an Ordered Triplet I
    - Maximum Value of an Ordered Triplet I 문제 풀이 및 해설 페이지 수정

--- solution/src/leetcode/2873/2873.ts
+++ solution/src/leetcode/2873/2873.ts
@@ -6,12 +6,12 @@ export function maximumTripletValue(nums: number[]): number {
   const n = nums.length;
 
   let answer = 0;
-  let maxi = Math.max(nums[0], nums[1]);
-  let maxij = nums[0] - nums[1];
+  let maxI = Math.max(nums[0], nums[1]);
+  let maxDiff = nums[0] - nums[1];
   for (let k = 2; k < n; k++) {
-    answer = Math.max(answer, maxij * nums[k]);
-    maxi = Math.max(maxi, nums[k - 1]);
-    maxij = Math.max(maxij, maxi - nums[k]);
+    answer = Math.max(answer, maxDiff * nums[k]);
+    maxDiff = Math.max(maxDiff, maxI - nums[k]);
+    maxI = Math.max(maxI, nums[k]);
   }
   return answer;
 }
