674f505bb5094d5e1b27902a0e4e809590c6cdbc
이상림 <contact@sangrim.dev>
2025-04-11 18:17:24 +0900

    :pencil2: Count Symmetric Integers
    - Count Symmetric Integers 문제 풀이 및 해설 페이지 수정

--- solution/src/leetcode/2843/2843.ts
+++ solution/src/leetcode/2843/2843.ts
@@ -3,25 +3,26 @@
  * https://leetcode.com/problems/count-symmetric-integers
  */
 export function countSymmetricIntegers(low: number, high: number): number {
-  function isSymmetric(num: number) {
-    const str = num.toString();
-    const n = str.length;
-    if (n % 2 !== 0) {
-      return false;
-    }
-    let [leftSum, rightSum] = [0, 0];
-    for (let i = 0; i < Math.floor(n / 2); i++) {
-      leftSum += parseInt(str[i]);
-      rightSum += parseInt(str[n - i - 1]);
-    }
-    return leftSum === rightSum;
-  }
-
   let answer = 0;
   for (let num = low; num <= high; num++) {
-    if (isSymmetric(num)) {
+    if (isSymmetricInteger(num)) {
       answer += 1;
     }
   }
   return answer;
+}
+
+function isSymmetricInteger(num: number): boolean {
+  const s = num.toString();
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+
+  const n = s.length / 2;
+  let [left, right] = [0, 0];
+  for (let i = 0; i < n; i++) {
+    left += parseInt(s[i]);
+    right += parseInt(s[s.length - i - 1]);
+  }
+  return left === right;
 }
